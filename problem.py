5185f5e7a57985abe4374d6065832a8999764123
ghastibasavaraj827-bit <ghastibasavaraj827@gmail.com>
2026-03-28 14:32:21 +0530

    Add files via upload

---
+++ problem.py
@@ -0,0 +1,34 @@
+# for i in range(1,5):
+#     if i==3:
+#         continue
+#     print(i)
+
+# for i in range(1,5):
+#     if i==3:
+#         break
+#     print(i)
+
+# list1=[10,20,30,40,50]
+# for i in list1:
+#     print(i)
+
+# mycart=[10,20,200,300,800,60,700]
+# for i in mycart:
+#     if i>400:
+#         print("This is my purchased cart")
+#         continue
+#     print(i)
+
+# write a program to calculate sum of list elements
+# list=[1,2,3,4,5,6,7,8,9,10]
+# sum=0
+# for i in list:
+#     sum+=i
+# print(f"The sum is {sum}")
+
+# name="Basavaraj"
+# newname=""
+# for i in name:
+#     newname=i+newname
+# print(newname)
+
